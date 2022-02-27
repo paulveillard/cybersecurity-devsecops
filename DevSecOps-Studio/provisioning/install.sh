9af544ec3992637d227ff76166bec3fd9297f67f
Paul Veillard, P. Eng <36570260+paulveillard@users.noreply.github.com>
2022-02-26 21:07:27 -0500

    Add files via upload

---
+++ DevSecOps-Studio/provisioning/install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -x
+
+TERRAFORM_VERSION="0.10.8"
+PACKER_VERSION="1.1.1"
+
+# create new ssh key
+[[ ! -f /home/ubuntu/.ssh/id_rsa ]] \
+&& mkdir -p /home/ubuntu/.ssh \
+&& ssh-keygen -f /home/ubuntu/.ssh/id_rsa -N '' \
+&& chown -R vagrant:vagrant /home/ubuntu/.ssh
+
+# install packages
+apt-get update
+apt-get -y install ansible unzip
+
+# Docker installation
+apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce
+
+# add docker privileges
+usermod -G docker vagrant 
+
+# install pip
+pip install -U pip && pip3 install -U pip
+if [[ $? == 127 ]]; then
+    wget -q https://bootstrap.pypa.io/get-pip.py
+    python get-pip.py
+    python3 get-pip.py
+fi
+
+# install awscli and ebcli
+pip install -U awscli
+pip install -U awsebcli
+
+#terraform
+T_VERSION=$(terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
+T_RETVAL=${PIPESTATUS[0]}
+
+[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
+&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+&& unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# packer
+P_VERSION=$(packer -v)
+P_RETVAL=$?
+
+[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
+&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
+&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
+&& rm packer_${PACKER_VERSION}_linux_amd64.zip
+
+# clean up
+apt-get clean
